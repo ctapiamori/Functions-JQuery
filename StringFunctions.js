9f0ba43fe0a40010e999fddf868ab347a87c74fa
ctapiamori <cesar.tapia@itcc.com.pe>
2014-06-15 02:37:01 -0500

    function equals

---
+++ StringFunctions.js
@@ -0,0 +1,11 @@
+$.fn.equals = function(compareTo) {
+  if (!compareTo || this.length != compareTo.length) {
+    return false;
+  }
+  for (var i = 0; i < this.length; ++i) {
+    if (this[i] !== compareTo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
